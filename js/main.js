f82fb752a32e8a6aff5feabc933421cc2df37e37
MD.JIHAD HOSSAIN <43439618+mdjihad560@users.noreply.github.com>
2022-05-07 23:35:48 +0600

    update

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function myFunction() {
+    var myVar1 = document.querySelector(".nav-toggler")
+    var myVar2 = document.querySelector(".menu")
+    myVar1.classList.toggle("active");
+    myVar2.classList.toggle("active");
+}
+
+
